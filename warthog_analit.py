63b8b67a8c94e59f7966a57bb2b51b21ba928e45
I-luk-I <105889683+I-luk-I@users.noreply.github.com>
2023-12-28 00:54:42 +0300

    Update warthog_analit.py

--- warthog_analit.py
+++ warthog_analit.py
@@ -1,17 +1,17 @@
 import requests
 url='http://188.234.5.83:3000'
-height=requests.get(f"{url}/chain/head").json()['data']['height']
+height = requests.get(f"{url}/chain/head").json()['data']['height']
 
-miners=f'{url}/chain/block/'
+miners = f'{url}/chain/block/'
           
-list_miners={}
+list_miners = {}
 
 for i in range (height-4000,height):
-    url_block_info=requests.get(f'{url}/chain/block/{i}').json()
+    url_block_info = requests.get(f'{url}/chain/block/{i}').json()
     if url_block_info['data']['body']['rewards'][0]['toAddress'] in list_miners:
-        list_miners[url_block_info['data']['body']['rewards'][0]['toAddress']]+=3
+        list_miners[url_block_info['data']['body']['rewards'][0]['toAddress']] += 3
     else:
-        list_miners[url_block_info['data']['body']['rewards'][0]['toAddress']]=3
+        list_miners[url_block_info['data']['body']['rewards'][0]['toAddress']] = 3
 with open ('values.txt','w') as file:
     file.write(str(list_miners.items()))
 
